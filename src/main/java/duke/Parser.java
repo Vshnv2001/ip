ea2eef664e1936f115f18fb9fa5d9906d1cf4dce
Vshnv2001 <mvaishnav.sg@gmail.com>
2022-09-07 22:07:40 +0800

    Fix bug in find method

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,6 +1,5 @@
 package duke;
 
-import java.util.Arrays;
 
 public class Parser {
     protected String commandParser(String command, TaskList taskList, UI ui) throws Exception {
@@ -16,9 +15,8 @@ public class Parser {
             int number = Integer.parseInt(command.split(" ")[1]);
             return taskList.deleteFromList(number, ui);
         } else if (command.split(" ")[0].toLowerCase().equals("find")) {
-            String[] keywords = command.split(" ", 6);
-            keywords = Arrays.copyOfRange(keywords, 1, keywords.length);
-            return taskList.findTasksByKeyword(ui, keywords);
+            String keyword = command.split(" ")[0];
+            return taskList.findTasksByKeyword(keyword, ui);
         } else {
             return taskList.addToList(command, ui);
         }
